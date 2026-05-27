f7732d4bbfb9ee0c72eab51f5c552dbe48d40bdf
DialloMamadouSaidou <mamadousaidoud466@gmail.com>
2026-05-27 09:23:22 -0400

    Ajout des input handler

--- lib/Input_handler/widgets/new_item.dart
+++ lib/Input_handler/widgets/new_item.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 
 import "../data/categories.dart";
+import "../models/category.dart";
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -10,6 +11,38 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final FocusNode _focusNode = FocusNode();
+  String entered_Name = "";
+  var entered_Quantity = 0;
+  bool _isFocused = false;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState(){
+    super.initState();
+
+    _focusNode.addListener((){
+      setState(() {
+         _isFocused = _focusNode.hasFocus;
+      });
+    });
+  }
+  void _saveItem() {
+
+       if(_formKey.currentState!.validate()){
+         _formKey.currentState!.save();
+       }
+
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,23 +50,34 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
+                focusNode: _focusNode,
+                style: TextStyle(fontSize: _isFocused ? 18 : 22, color: Colors.white),
                 maxLength: 50,
-
-                decoration: const InputDecoration(label: Text("Name")),
+                minLines: 1,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: const InputDecoration(label: Text("Name"),
+                labelStyle: TextStyle(fontSize: 18, color: Colors.white)
+                ),
                 validator: (value) {
                   if(value == null || value.isEmpty || value.trim().length <= 1 || value.trim().length > 50){
                     return "Must be between 1 and 50 characters";
                   }
                   return null;
                 },
+                onSaved: (value){
+                    entered_Name = value!;
+                },
               ),
               Row(
                 children: [
                   Expanded(
                     child: TextFormField(
+                      keyboardType: TextInputType.number,
                       decoration: InputDecoration(label: Text("Quantity")),
                       initialValue: "1",
                       validator: (value) {
@@ -42,11 +86,15 @@ class _NewItemState extends State<NewItem> {
                         }
                         return null;
                       },
+                      onSaved: (value){
+                        entered_Quantity = int.parse(value!);
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
                   Expanded(
                     child: DropdownButtonFormField(
+                      initialValue: _selectedCategory,
                       items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
@@ -65,7 +113,9 @@ class _NewItemState extends State<NewItem> {
                             ),
                           ),
                       ],
-                      onChanged: (v) {},
+                      onChanged: (v) {
+                          print(v);
+                      },
                     ),
                   ),
                 ],
@@ -73,8 +123,10 @@ class _NewItemState extends State<NewItem> {
               Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextButton(onPressed: (){}, child: const Text("Reset"),),
-                  ElevatedButton(onPressed: (){},
+                  TextButton(onPressed: (){
+                     _formKey.currentState!.reset();
+                  }, child: const Text("Reset"),),
+                  ElevatedButton(onPressed: _saveItem,
                     child: Text("Add Item")
                   )
                 ]
